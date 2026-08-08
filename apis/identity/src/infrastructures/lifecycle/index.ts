a93a36504e858a3625faff26d2762cc6c80037f3
Jesús Ángel Rodríguez Martínez <jesus.arm.dev@gmail.com>
2026-08-07 22:52:45 -0700

    feat: :sparkles: add helper

--- apis/identity/src/infrastructures/lifecycle/index.ts
+++ apis/identity/src/infrastructures/lifecycle/index.ts
@@ -1,2 +1,2 @@
-export * from './services';
+export * from './helpers';
 export * from './types';
--- libs/api/src/infrastructures/index.ts
+++ libs/api/src/infrastructures/index.ts
@@ -1,4 +1,3 @@
 export * from './apis';
-export * from './authentication-middlewares';
 export * from './global-middlewares';
 export * from './paths';
